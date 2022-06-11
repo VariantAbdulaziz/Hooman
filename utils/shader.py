334adabc935cf353284193d8ef25f586e64d2774
Abdulaziz Ali <s9uidward@gmail.com>
2022-06-11 15:33:43 +0300

    implemented material and shader classes

--- utils/shader.py
+++ utils/shader.py
@@ -1,15 +1,30 @@
+from OpenGL.GL import *
+from OpenGL.GL.shaders import *
+import os
+
+
 class Shader:
     def getFileContents(filename):
-        pass
+        p = os.path.join(os.getcwd(), "shaders", filename)
+        return open(p, 'r').read()
 
     def __init__(self, vert_file_path, fragment_file_path) -> None:
-        pass
+        vertexShaderContent = Shader.getFileContents(vert_file_path)
+        fragmentShaderContent = Shader.getFileContents(fragment_file_path)
+
+        vertexShader = compileShader(vertexShaderContent, GL_VERTEX_SHADER)
+        fragmentShader = compileShader(fragmentShaderContent, GL_FRAGMENT_SHADER)
+
+        self.program = glCreateProgram()
+        glAttachShader(self.program, vertexShader)
+        glAttachShader(self.program, fragmentShader)
+        glLinkProgram(self.program)
 
     def getId(self):
-        pass
+        return self.program
 
     def use(self):
-        pass
+        glUseProgram(self.program)
 
     def delete(self):
-        pass
+        glDeleteProgram(self.program)
